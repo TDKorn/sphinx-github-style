0535726aef663f1276deece68050e75c8a0725ee
TDKorn <96394652+TDKorn@users.noreply.github.com>
2023-02-19 06:40:56 -0500

    Update linkcode_resolve.py

--- sphinx_github_style/linkcode_resolve.py
+++ sphinx_github_style/linkcode_resolve.py
@@ -1,7 +1,26 @@
-#! sphinx_github_style/linkcode_resolve.py
+# ============================ Linkcode Extension Settings ============================
+#
+#                     Adapted from https://github.com/nlgranger/SeqTools
+#
+#
+import inspect
+import os
+import subprocess
+import sys
+
+import pkg_resources
 
 # Get the blob to link to on GitHub
 linkcode_revision = "master"
+
+# Get the most recent tag to link to on GitHub
+# try:
+#     cmd = "git describe --tags --abbrev=0"
+#     tag = subprocess.check_output(cmd.split(" ")).strip().decode('utf-8')
+#     linkcode_revision = tag
+#
+# except subprocess.CalledProcessError:
+#     linkcode_revision = "main"
 
 try:
     # lock to commit number
@@ -20,24 +39,25 @@ try:
     tag = subprocess.check_output(cmd.split(" ")).strip().decode('utf-8')
     linkcode_revision = tag
 
+
 except subprocess.CalledProcessError:
     pass
 
 
-# Add the "Edit on GitHub" link at the top
-html_context = {
-    'display_github': True,
-    'github_user': 'TDKorn',
-    'github_repo': 'insta-tweet',
-    'github_version': f'{linkcode_revision}/docs/source/'
-}
+# Set GitHub version to be same as linkcode
+html_context['github_version'] = linkcode_revision
 
 # Source URL template; formatted + returned by linkcode_resolve
-linkcode_url = "https://github.com/tdkorn/insta-tweet/blob/" \
+linkcode_url = "https://github.com/tdkorn/my-magento/blob/" \
                + linkcode_revision + "/{filepath}#L{linestart}-L{linestop}"
+"https://github.com/{user}/{repo}/tree/{linkcode_revision}/{pkg}/{file}.py#L{start}-L{finish}}"
 
-# Hardcoded Top Level Module Path // since InstaTweet isn't PyPi release name :(  it could be tho...
-modpath = pkg_resources.require('insta-tweet')[0].location
+# Hardcoded Top Level Module Path since MyMagento isn't PyPi release name
+modpath = pkg_resources.require('my-magento')[0].location
+
+# Top Level Package Name
+top_level = 'magento'  # pkg_resources.require('my-magento')[0].get_metadata('top_level.txt').strip()
+
 
 
 def linkcode_resolve(domain, info):
@@ -85,26 +105,23 @@ def linkcode_resolve(domain, info):
         linestart, linestop = lineno, lineno + len(source) - 1
 
     # Format link using the filepath of the source file plus the line numbers
-    if on_rtd:
-        # Ex. InstaTweet\instatweet.py --> InstaTweet/instatweet.py
-        filepath = '/'.join(filepath.split('\\'))
+    # Fix links with "../../../" or "..\\..\\..\\"
+    filepath = '/'.join(filepath[filepath.find(top_level):].split('\\'))
 
-    else:
-        # For local builds, I must be doing something wrong because the
-        # paths are always like "..\..\..\InstaTweet\module.py"
-        filepath = '/'.join(filepath.lstrip('..\\').split('\\'))
-
-    # Example of final link: https://github.com/tdkorn/insta-tweet/blob/docs/InstaTweet/instatweet.py#L73-L117
+    # Example of final link: # https://github.com/tdkorn/my-magento/blob/sphinx-docs/magento/utils.py#L355-L357
     final_link = linkcode_url.format(
         filepath=filepath,
         linestart=linestart,
         linestop=linestop
     )
     print(f"Final Link for {fullname}: {final_link}")
-    replace_autodoc_refs_with_linkcode(
-        info=info,
-        link=final_link,
-        rst_src=rst_src,
-    )
-    return final_link
 
+    # Use the link to replace directives with links in the README for GitHub/PyPi
+    # if not on_rtd:
+    #     for rst_src in rst_sources:
+    #         replace_autodoc_refs_with_linkcode(
+    #             info=info,
+    #             link=final_link,
+    #             rst_src=rst_src
+    #         )
+    return final_link
