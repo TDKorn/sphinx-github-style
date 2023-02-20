897df4f31953a18ccca14928da54a050ad9fef6a
TDKorn <96394652+TDKorn@users.noreply.github.com>
2023-02-19 21:07:23 -0500

    Update meth_lexer.py

--- sphinx_github_style/meth_lexer.py
+++ sphinx_github_style/meth_lexer.py
@@ -1,32 +1,11 @@
 from pygments.lexers.python import NumPyLexer
 from inspect import getmembers, getmodule, isfunction, ismethod, ismodule, isclass
 from sphinx.application import Sphinx
-import types
 import sphinx_github_style
-
-def get_pkg_funcs(pkg: types.ModuleType):
-    funcs_meths = get_funcs(pkg)  # Get funcs/meths defined in pkg.__init__
-    modules = getmembers(pkg, ismodule)  # Modules of package
-    for name, module in modules:
-        funcs_meths += get_funcs(module)  # Get standalone funcs defined in module
-        classes = getmembers(module, isclass)  # Get classes in module
-        for class_name, _class in classes:
-            if getmodule(_class).__name__.startswith(
-                    pkg.__name__):  # If class is defined in the module, get its funcs/meths
-                funcs_meths += get_funcs(_class)
-    return set(funcs_meths)
-
-
-def get_funcs(of):
-    members = getmembers(of, isfunction or ismethod)
-    return list(dict(members))
-
-
-funcs = get_pkg_funcs(sphinx_github_style)
+import types
 
 
 class TDKMethLexer(NumPyLexer):
-
     """Adds syntax highlighting for a python Package's methods
 
     """
@@ -34,7 +13,31 @@ class TDKMethLexer(NumPyLexer):
     url = 'https://github.com/TDKorn'
     aliases = ['tdk']
 
-    EXTRA_KEYWORDS = NumPyLexer.EXTRA_KEYWORDS.union(funcs)
+    def __int__(self):
+        funcs = TDKMethLexer.get_pkg_funcs(sphinx_github_style)
+        funcs.update(NumPyLexer.EXTRA_KEYWORDS)
+        TDKMethLexer.EXTRA_KEYWORDS = funcs
+        print(TDKMethLexer.EXTRA_KEYWORDS)
+        print(self.EXTRA_KEYWORDS)
+
+    @staticmethod
+    def get_funcs(of):
+        members = getmembers(of, isfunction or ismethod)
+        return list(dict(members))
+
+    @staticmethod
+    def get_pkg_funcs(pkg: types.ModuleType):
+        # Get funcs/meths defined in pkg.__init__
+        funcs_meths = TDKMethLexer.get_funcs(pkg)
+        modules = getmembers(pkg, ismodule)
+        # Get funcs/meths of each module in pkg
+        for name, module in modules:
+            funcs_meths += TDKMethLexer.get_funcs(module)
+            classes = getmembers(module, isclass)
+            for class_name, _class in classes:
+                funcs_meths += TDKMethLexer.get_funcs(_class)
+        # Set of all funcs/meths contained in modules used by package
+        return set(funcs_meths)
 
 
 def setup(app: Sphinx):
