f5204fb3ab28e41ba42b46259de78c6f8cf9d918
TDKorn <96394652+TDKorn@users.noreply.github.com>
2023-02-19 22:53:25 -0500

    cd

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from setuptools import setup, find_packages
 
-LONG_DESCRIPTION_SRC = 'README_PyPi.rst'
+LONG_DESCRIPTION_SRC = 'README.rst'
 
 
 def read(file):
@@ -20,30 +20,19 @@ setup(
     name="sphinx-github-style",
     version=version,
     description="Sphinx Github Integration and Github Dark Theme Pygments Style",
-    # long_description=read(LONG_DESCRIPTION_SRC),
-    # long_description_content_type="text/x-rst; charset=UTF-8",
+    long_description=read(LONG_DESCRIPTION_SRC),
+    long_description_content_type="text/x-rst; charset=UTF-8",
     author="Adam Korn",
     author_email='hello@dailykitten.net',
     license="MIT License",
     packages=find_packages(),
+    keywords=["sphinx", "sphinx-extension", "sphinx-theme", "pygments", "pygments-style", "github", "linkcode"],
     url="https://github.com/tdkorn/sphinx-github-style",
     download_url="https://github.com/TDKorn/sphinx-github-style/tarball/master",
-    keywords=["sphinx", "sphinx-extension", "sphinx-theme", "pygments", "pygments-style", "github", "linkcode"],
-    # install_requires=["requests"],
     package_data={
         "sphinx_github_style": [
-            "_static/github-linkcode.css",
-            "add_linkcode_class.py",
-             "github_style.py",
-            "linkcode_resolve.py",
-        # "sphinx_copybutton": [
-        #     "_static/copybutton.css",
-        #     "_static/copybutton_funcs.js",
-        #     "_static/copybutton.js_t",
-        #     "_static/copy-button.svg",
-        #     "_static/check-solid.svg",
-        #     "_static/clipboard.min.js",
-        ]
+            "_static/github_linkcode.css",
+            ],
     },
     classifiers=[
         "Framework :: Sphinx :: Extension",
